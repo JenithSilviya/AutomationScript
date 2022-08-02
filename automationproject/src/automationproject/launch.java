60053aec2b967c14938773608dd0d3c059ed4aeb
jenith.silviya <jenith.silviya@QT-INBG-LT0282.QUALITEST.LOCAL>
2022-08-02 16:04:26 +0530

    launch

--- automationproject/src/automationproject/launch.java
+++ automationproject/src/automationproject/launch.java
@@ -4,18 +4,18 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.Test;
 
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+@Test
 public class launch
 {
 WebDriver driver;
-@Test
-public void Lanching_the  Browser()
+   public void Lanching_the_Browser()
 {
-System.setProperty("webdriver.chrome.driver","C:\\Users\\jenith.silviya\\Downloads\\chromedriver_win32\\chromedriver.exe");
-WebDriverManager.chromedriver().setup();
-driver=new ChromeDriver();
-driver.get("https://mobileworld.azurewebsites.net/");
-driver.manage().window().maximize();
-
-
+//System.setProperty("webdriver.chrome.driver","C:\\Users\\jenith.silviya\\Downloads\\chromedriver_win32\\chromedriver.exe");
+  WebDriverManager.chromedriver().setup();
+   driver=new ChromeDriver();
+   driver.get("https://mobileworld.azurewebsites.net/");
+   driver.manage().window().maximize();
 }
 }
