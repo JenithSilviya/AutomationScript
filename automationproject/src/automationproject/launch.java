6e8584c9efd1c456146ee1079e156b2ed09d8cbe
jenith.silviya <jenith.silviya@QT-INBG-LT0282.QUALITEST.LOCAL>
2022-08-02 12:44:30 +0530

    launch

--- automationproject/src/automationproject/launch.java
+++ automationproject/src/automationproject/launch.java
@@ -6,12 +6,13 @@ import org.testng.annotations.Test;
 
 public class launch
 {
-
-@Test 
-public void launch
+WebDriver driver;
+@Test
+public void Lanching_the  Browser()
 {
 System.setProperty("webdriver.chrome.driver","C:\\Users\\jenith.silviya\\Downloads\\chromedriver_win32\\chromedriver.exe");
-WebDriver driver=new ChromeDriver();
+WebDriverManager.chromedriver().setup();
+driver=new ChromeDriver();
 driver.get("https://mobileworld.azurewebsites.net/");
 driver.manage().window().maximize();
 
